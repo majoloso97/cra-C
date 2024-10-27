c98c95db41bebe479abaedad66ee29118d267a3c
Marvin López Osorio <majoloso97@gmail.com>
2024-10-27 23:52:03 +0100

    style: clang formatting

--- CS50/PS-1/1-hello-me/hello.c
+++ CS50/PS-1/1-hello-me/hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void) {
-   string name = get_string("What's your name? ");
-   printf("Hello, %s!\n", name);
+  string name = get_string("What's your name? ");
+  printf("Hello, %s!\n", name);
 }
