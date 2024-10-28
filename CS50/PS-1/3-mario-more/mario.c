bfe9d9b755580e818a98c3c39c944d4c1c03ade5
Marvin López Osorio <majoloso97@gmail.com>
2024-10-28 09:15:18 +0100

    feat: Mario assignment second version

---
+++ CS50/PS-1/3-mario-more/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  while (n < 1) {
+    n = get_int("Height: ");
+  }
+  string sep = "  ";
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      // Add padding equal to row's length - base length
+      printf(" ");
+    }
+    for (int j = 0; j < i; j++) {
+      printf("#");
+    }
+    printf("%s", sep);
+
+    for (int j = 0; j < i; j++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
